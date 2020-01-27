8b41d35943a3332feb311ca321dd9b2773d20e4d
Serge <kims230599@gmail.com>
2020-01-27 23:57:08 +0300

    tf2 experiements

---
+++ models/base_model.py
@@ -0,0 +1,20 @@
+import tensorflow as tf
+from tensorflow.keras.layers import Conv2D, Flatten, Dense
+from tensorflow.keras import Model
+
+
+class BaseModel(Model):
+    def __init__(self):
+        super(BaseModel, self).__init__()
+        self.conv1 = Conv2D(32, 3, activation="relu")
+        self.flatten = Flatten()
+        self.d1 = Dense(128, activation="relu")
+        self.d2 = Dense(10, activation="softmax")
+
+    def call(self, x):
+        x = self.conv1(x)
+        x = self.flatten(x)
+        x = self.d1(x)
+        x = self.d2(x)
+        return x
+
