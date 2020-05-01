c56f74ac957ad33280fde885c8270c963ab54da8
Serge <kims230599@gmail.com>
2020-05-01 20:40:45 +0300

    paths in args, redesigned saves

--- train.py
+++ train.py
@@ -47,14 +47,11 @@ def train(args, model, device, train_loader_a, train_loader_b, optimizer, epoch)
         loss.backward()
         optimizer.step()
 
-        if args.verbose:
-            print(epoch, batch_index, metrics_dict)
-
-        if args.save_model and batch_index % 20 == 0:
-            torch.save(model.state_dict(), './weights/last.hdf5') #TODO paths for checkpoints
-            torch.save({'model_state_dict': model.state_dict(),
-                        'optimizer_state_dict': optimizer.state_dict()},
-                        './weights/{}_v5_{}_{}.hdf5'.format(args.model, epoch, batch_index))
+        if batch_index % 100 == 0:
+            if args.verbose:
+                print(epoch, batch_index, metrics_dict)
+            if args.save_model:
+                torch.save(model.state_dict(), "{}/last_model_only.hdf5".format(args.weights_path))
 
     print("The training epoch ended in {} seconds".format(time.time() - time_start))
 
@@ -86,8 +83,11 @@ def main():
 
     optimizer = torch.optim.Adam(model.parameters(), lr=3e-4)
 
-    subject_filenames = filter_filenames(paths=[str(p) for p in Path("./data/subject_images").glob("*.jpg")], limit=SUBJECT_SIZE)
-    astigma_filenames = filter_filenames(paths=[str(p) for p in Path("./data/astigma_images").glob("*.jpg")], limit=ASTIGMA_SIZE)
+    subject_filenames = [str(p) for p in Path(args.subject_images_path).glob("*.jpg")]
+    astigma_filenames = [str(p) for p in Path(args.astigma_images_path).glob("*.jpg")]
+
+    subject_filenames = filter_filenames(paths=subject_filenames, limit=SUBJECT_SIZE)
+    astigma_filenames = filter_filenames(paths=astigma_filenames, limit=ASTIGMA_SIZE)
 
     subject_filenames = np.array(MULTI_REFLECTION * subject_filenames)
     astigma_filenames = np.array(2 * MULTI_REFLECTION * astigma_filenames)
@@ -118,6 +118,11 @@ def main():
     for epoch in range(args.n_epochs):
         print(epoch)
         train(args, model, device, train_loader_a, train_loader_b, optimizer, epoch)
+        if args.save_model:
+            torch.save({'model_state_dict': model.state_dict(),
+                        'optimizer_state_dict': optimizer.state_dict()},
+                        "{}/{}_v5_e{}.hdf5".format(args.weights_path, args.model, epoch, batch_index))
+
 
 
 if __name__ == "__main__":
--- utils/args.py
+++ utils/args.py
@@ -4,13 +4,18 @@ from pathlib import Path
 
 def train_parse_args():
     parser = argparse.ArgumentParser()
-    parser.add_argument("--model", default="unet", type=str, help="model type")
-    parser.add_argument("--logs_path", default="")  # TODO where must we use it?
-    parser.add_argument("--batch-size", default=2, type=int, help="batch_size")
-    parser.add_argument("--n-epochs", default=10, type=int, help="number of epochs")
+    parser.add_argument("--model", default="unet", type=str, help="model type, default: unet")
+    parser.add_argument("--batch-size", default=2, type=int, help="batch_size, default: 2")
+    parser.add_argument("--n-epochs", default=10, type=int, help="number of epochs, default: 10")
+
     parser.add_argument("--disable-cuda", action='store_true', help="disable CUDA")
-    parser.add_argument("--save-model", action='store_true', help="save_model")
+    parser.add_argument("--save-model", action='store_true', help="save model")
     parser.add_argument("--verbose", action='store_true', help="verbose")
+
+    parser.add_argument("--subject-images-path", default="{}/data/subject_images".format(Path.cwd()), type=str, help="subject images path")
+    parser.add_argument("--astigma-images-path", default="{}/data/astigma_images".format(Path.cwd()), type=str, help="astigma images path")
+    parser.add_argument("--weights-path", default="{}/weights".format(Path.cwd()), type=str, help="weigths path")
+    parser.add_argument("--logs-path", default="{}/logs".format(Path.cwd()), type=str, help="logs path")
 
     return parser.parse_args()
 
